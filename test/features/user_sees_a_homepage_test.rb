0a0da78ae530ac0ef070352efda731555385cdce
Oscar Bellechasse <obellech10@gmail.com>
2019-06-16 14:48:23 -0600

    Add test for page that doesnt exist

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -3,8 +3,15 @@ require './test/test_helper'
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
-    
+
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
+  end
+
+  def test_user_visits_page_that_doesnt_exist
+    visit 'somepage.com'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
   end
 end
