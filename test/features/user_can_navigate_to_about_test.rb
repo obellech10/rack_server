4a2b0e93ed5ab13da76827596573b08f0f0e31e3
Oscar Bellechasse <obellech10@gmail.com>
2019-06-17 09:26:06 -0600

    Add test for linking to blog page

--- test/features/user_can_navigate_to_about_test.rb
+++ test/features/user_can_navigate_to_about_test.rb
@@ -9,4 +9,13 @@ class LinkTest < CapybaraTestCase
     assert_equal '/about', current_path
     assert page.has_content?("About Me!")
   end
+
+  def test_user_can_see_the_blog_page
+    visit '/'
+    click_on "Blog"
+
+    assert_equal 200, page.status_code
+    assert_equal '/blog', current_path
+    assert page.has_content?("My Blog")
+  end
 end
